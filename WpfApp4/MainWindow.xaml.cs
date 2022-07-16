a558a22adbec736e22b7a76f687b76f0fa690781
Dell <Dell@DESKTOP-B5UG1VL>
2022-07-16 18:23:46 +0200

    Dodaj pliki projektów.

---
+++ WpfApp4/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public int Counter { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            Counter = 0;
+
+            /* 
+             * Button a = new Button();
+             a.Content = "Test";
+             siatka.Children.Add(a);
+            */
+        }
+        /*
+       * private void add_Click(object sender, RoutedEventArgs e)
+       {
+           label_content.Content = Counter++;
+       }
+      */
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            //test.Text = "przetestowane";
+            MessageBox.Show("TEST");
+        }
+    }
+}
